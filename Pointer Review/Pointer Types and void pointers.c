9408f6fd33eddb9cc9b3c647610a6607c78e31a5
karketh <andrewholmes@knights.ucf.edu>
2022-05-18 13:13:15 -0400

    Typecasting
    Added typecasting in this example

--- Pointer Review/Pointer Types and void pointers.c
+++ Pointer Review/Pointer Types and void pointers.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 int types() {
-	int* a = 1025;
-	int p = &a;
+	int a = 1025;
+	int *p = &a;
+	
 
 
-	printf("%d",&p);
+	printf("Size of integer is: %d\n", sizeof(p));
 
+	printf("Address = %p, value = %d\n", p, *p);
+	char hi;
+	char *p0;
 
+	p0 = (char *)  p;
 
+	printf("Size of char is: %d\n", sizeof(p0));
 
+	printf("Address = %p, value = %d\n", p0, *p0);
+	
 	return NULL;
 }
--- Pointer Review/Source.c
+++ Pointer Review/Source.c
@@ -3,10 +3,7 @@
 int main()
 {
 
-
 	types();
-
-
 
 	return 0;
 }
